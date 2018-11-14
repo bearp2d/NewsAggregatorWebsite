22788c5b48bd1f15bcf0c19e8e7ecec6cddd3546
bearp2d <bearp2d@gmail.com>
2018-11-14 14:59:14 -0500

    Remember to rebase and rebranch

---
+++ frontend/components/landing/signup_form_container.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Route, Redirect, Switch, Link, HashRouter } from 'react-router-dom';
+
+import sessionForm from './session_form';
